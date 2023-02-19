787fb738757edde7b394bb88ba3caae5b4480b74
Quincy Owyang <102776831+0uincy@users.noreply.github.com>
2023-02-19 15:45:19 -0800

    added manual control

--- Zero_Function/Zero_Function.ino
+++ Zero_Function/Zero_Function.ino
@@ -11,10 +11,11 @@ const int runPin = 6;         //run zero function
 int dir = 0;    //direction status
 int on = 0;     //on/off status
 int limit = 0;  //motor limit status
-int hasRun = 0; //if function has been run already = 0;
+int runZero = 0;
+int hasRun = 1; //if function has been run already = 0;
 
 
-const int zconv = 200/4;  //n steps per n mm
+const int zconv = 200/8;  //n steps per n mm
 int offset = 3;           //offset movement in mm
 int zmax = 463;           //final height
 
@@ -31,9 +32,31 @@ void setup()
 
 void loop() 
 {
-  while (hasRun == 0)
-  {
-    hasRun = digitalRead(runPin);
+  runZero = digitalRead(runPin);
+
+  while (runZero == 0)
+  { 
+    //set direction of motor
+    if(dir == HIGH)
+    {
+      digitalWrite(dirPin, HIGH); //set motor direction up         
+    }
+    
+    else
+    {
+      digitalWrite(dirPin, LOW);  //set motor direction down
+    }
+      
+    //step motor once
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(400);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(400);
+
+    //read input pin values
+    dir = digitalRead(dircontrolPin);
+    on = digitalRead(onPin);
+    runZero = digitalRead(runPin);
   }
 
   //read input pin values
@@ -41,63 +64,67 @@ void loop()
   dir = digitalRead(dircontrolPin);
   limit = digitalRead(limitPin);
 
-  //move motor down until limit switch activation
-  while(limit == LOW)
-  { 
-    digitalWrite(dirPin, LOW);
+  while(hasRun = 0)
+  {
+    //move motor down until limit switch activation
+    while(limit == LOW)
+    { 
+      digitalWrite(dirPin, LOW);
       
-    //step motor once
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(400);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(400);
+      //step motor once
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(400);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(400);
 
-    limit = digitalRead(limitPin);
-  }
+      limit = digitalRead(limitPin);
+    }
   
-  delay(250);
+    delay(250);
 
-  //move motor vertically offset distance
-  for (int i = 0; i < offset * zconv; i++)
-  {
-    digitalWrite(dirPin, HIGH);
+    //move motor vertically offset distance
+    for (int i = 0; i < offset * zconv; i++)
+    {
+      digitalWrite(dirPin, HIGH);
     
-    //step motor once
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(400);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(400);
-  }
-
-  limit = digitalRead(limitPin);
-
-  //move motor slowly to zero point
-  while(limit == LOW)
-  { 
-    digitalWrite(dirPin, LOW);
-      
-    //step motor once
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(4000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(4000);
+      //step motor once
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(400);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(400);
+    }
 
     limit = digitalRead(limitPin);
-  }
 
-  delay(1000);
+    //move motor slowly to zero point
+    while(limit == LOW)
+    { 
+      digitalWrite(dirPin, LOW);
+      
+      //step motor once
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(4000);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(4000);
 
-  //move motor vertically offset distance
-  for (int i = 0; i < zmax * zconv; i++)
-  {
-    digitalWrite(dirPin, HIGH);
+      limit = digitalRead(limitPin);
+    }
+
+    delay(1000);
+
+    //move motor vertically to zmax
+    for (int i = 0; i < zmax * zconv; i++)
+    {
+      digitalWrite(dirPin, HIGH);
     
-    //step motor once
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(400);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(400);
+      //step motor once
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(400);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(400);
+    }
+
+    hasRun = 1;
   }
 
-  hasRun = 0;
 }
